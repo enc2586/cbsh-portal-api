62beed3364eaf0cb026455039f8a999bc7cdde57
enc2586 <enc2586@gmail.com>
2022-03-10 22:26:28 +0900

    v1 finished

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ from bs4 import BeautifulSoup
 SIGNIN_URL = "http://academic.petapop.com/sign/actionLogin.do"
 APPLY_URL = "http://academic.petapop.com/self/requestSelfLrn.do"
 DELETE_URL = "http://academic.petapop.com/self/deleteSelfLrn.do"
+TCRINFO_URL = "http://academic.petapop.com/self/writeSelfLrnReqst.do"
+ROOMINFO_URL = "http://academic.petapop.com/clssrm/buldDrw.do"
+SEATINFO_URL = "http://academic.petapop.com/clssrm/seatInfo.json"
+LRNSTAT_URL = "http://academic.petapop.com/self/mainSelfLrnReqst.do"
 
 KST = pytz.timezone('Asia/Seoul')
 
@@ -29,11 +33,11 @@ CORS(app)
 
 @app.route('/', methods=['GET'])
 def root():
-    return "Hello, world!"
+    return json.dumps(True)
 
 
 @app.route('/legacySelfLearn/credentialValidity', methods=['POST'])
-def applySelfLearn():
+def checkCredentialValidity():
     body = flask.request.json
 
     userData = {
@@ -52,57 +56,105 @@ def applySelfLearn():
             return json.dumps(True)
 
 
-@app.route('/legacySelfLearn/selfLearn', methods=['PUT'])
+@app.route('/legacySelfLearn/selfLearn', methods=['PUT', 'DELETE'])
 def applySelfLearn():
     body = flask.request.json
 
-    userData = {
-        "id": body['id'],
-        "password": body['password']
-    }
+    if flask.request.method == 'PUT':
+        if not all(item in body.keys() for item in ["id", "password", "homeRoomTeacherCode", "conductingTeacherCode", "classroomCode", "period"]):
+            return flask.make_response("Invalid request body.", 400)
 
-    applyData = {
-        'roomTcherId': body['homeRoomTeacherCode'],
-        'cchTcherId': body['conductingTeacherCode'],
-        'clssrmId': body['classroomCode'],
-        'actCode': body['actCode'],
-        'actCn': body['actContent'],
-        'sgnId': kstNow().strftime(r"%Y%m%d")
-    }
+        userData = {
+            "id": body['id'],
+            "password": body['password']
+        }
 
-    with req.session() as sess:
-        res = sess.post(SIGNIN_URL, data=userData)
-        rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
+        applyData = {
+            'roomTcherId': body['homeRoomTeacherCode'],
+            'cchTcherId': body['conductingTeacherCode'],
+            'clssrmId': body['classroomCode'],
+            'actCode': body['actCode'] if 'actCode' in body else "ACT999",
+            'actCn': body['actContent'] if 'actCn' in body else ".",
+            'sgnId': body['date'] if 'date' in body else kstNow().strftime(r"%Y%m%d")
+        }
 
-        if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
-            return flask.make_response("Record not found", 400)
-
-        else:
-            result = []
-
+        with req.session() as sess:
             for period in body['periods']:
-                applyCaseData = applyData.copy()
-                applyCaseData['lrnPd'] = period
+                if type(period) != int:
+                    return flask.make_response("Period data includes non-int values.", 400)
 
-                res = sess.post(APPLY_URL, data=applyCaseData)
-                response = json.loads(res.content.decode('utf-8'))
+            res = sess.post(SIGNIN_URL, data=userData)
+            rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
 
-                if response['result']['success'] == True:
-                    result.append(response['slrnNo'])
+            if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
+                return flask.make_response("Invalid credentials", 400)
 
-                else:
-                    result.append(False)
+            else:
+                result = {}
 
-            return result
+                for period in body['periods']:
+                    applyCaseData = applyData.copy()
+                    applyCaseData['lrnPd'] = period
+
+                    res = sess.post(APPLY_URL, data=applyCaseData)
+                    response = json.loads(res.content.decode('utf-8'))
+
+                    if response['result']['success'] == True:
+                        result[period] = response['slrnNo']
+
+                    else:
+                        result[period] = False
+
+                return json.dumps(result)
+
+    elif flask.request.method == 'DELETE':
+        if not all(item in body.keys() for item in ["id", "password", "target"]):
+            return flask.make_response("Invalid request body.", 400)
+
+        userData = {
+            "id": body['id'],
+            "password": body['password']
+        }
+
+        with req.session() as sess:
+            for serial in body['target']:
+                if type(serial) != int:
+                    return flask.make_response("Target data includes non-int values.", 400)
+
+            res = sess.post(SIGNIN_URL, data=userData)
+            rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
+
+            if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
+                return flask.make_response("Invalid Credentials", 400)
+
+            else:
+                result = {}
+                for serial in body['target']:
+                    deleteData = {
+                        "slrnNo": serial
+                    }
+                    res = sess.post(DELETE_URL, data=deleteData)
+                    response = json.loads(res.content.decode('utf-8'))
+
+                    if response['result']['success'] == True:
+                        result[serial] = True
+
+                    else:
+                        result[serial] = False
+
+                return json.dumps(result)
 
 
-@app.route('/legacySelfLearn/selfLearn', methods=['DELETE'])
-def deleteSelfLearn():
+@app.route('/legacySelfLearn/teacherCodes', methods=['GET'])
+def getTeacherCodes():
     body = flask.request.json
 
+    if not all(item in body.keys() for item in ["id", "password"]):
+        return flask.make_response("Invalid request body.", 400)
+
     userData = {
         "id": body['id'],
-        "password": body['password']
+        "password": body['password'],
     }
 
     with req.session() as sess:
@@ -110,22 +162,65 @@ def deleteSelfLearn():
         rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
 
         if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
-            return flask.make_response("Record not found", 400)
+            return flask.make_response("Invalid credentials", 400)
 
         else:
-            result = []
+            res = sess.get(TCRINFO_URL)
+            pageData = BeautifulSoup(
+                res.content.decode('utf-8'), "html.parser")
 
-            for serial in body['target']:
-                deleteData = {
-                    "slrnNo": serial,
-                }
-                res = sess.post(DELETE_URL, data=deleteData)
-                response = json.loads(res.content.decode('utf-8'))
+            teacherData = {}
+            for element in pageData.find_all('option'):
+                if element['value']:
+                    teacherData[element.get_text()] = element['value']
 
-                if response['result']['success'] == True:
-                    result.append(True)
+            if not teacherData:
+                return json.dumps(False)
 
-                else:
-                    result.append(False)
+            teacherData['지도교사없음'] = ''
+            return json.dumps(teacherData)
 
-            return result
+
+@app.route('/legacySelfLearn/classDatas', methods=['GET'])
+def getClassDatas():
+    body = flask.request.json
+
+    if not all(item in body.keys() for item in ["id", "password"]):
+        return flask.make_response("Invalid request body.", 400)
+
+    userData = {
+        "id": body['id'],
+        "password": body['password'],
+    }
+
+    with req.session() as sess:
+        res = sess.post(SIGNIN_URL, data=userData)
+        rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
+
+        if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
+            return flask.make_response("Invalid credentials", 400)
+
+        else:
+            res = sess.get(ROOMINFO_URL)
+            response = BeautifulSoup(
+                res.content.decode('utf-8'), "html.parser")
+
+            rawClassList = response.select(
+                'div > div > div > div.data-list.custom-list > table > tbody > tr')
+
+            classData = {}
+            for rawClassHTML in rawClassList:
+                rawClassData = rawClassHTML.select('td')
+                name = rawClassData[1].get_text()
+                if not ('삭제' in name):
+                    classData[name] = {
+                        "floor": rawClassData[0].get_text(),
+                        "maxppl": cleanUp(rawClassData[3].get_text()),
+                        "tcher": rawClassData[2].get_text(),
+                        "id": rawClassData[4].select_one('div > input')['value'],
+                    }
+
+            if not classData:
+                return json.dumps(False)
+
+            return json.dumps(classData)
--- temp.py
+++ temp.py
@@ -1,90 +1,90 @@
-from datetime import datetime
-import pytz
-import requests as req
-from bs4 import BeautifulSoup
-import json
+# from datetime import datetime
+# import pytz
+# import requests as req
+# from bs4 import BeautifulSoup
+# import json
 
-SIGNIN_URL = "http://academic.petapop.com/sign/actionLogin.do"
-APPLY_URL = "http://academic.petapop.com/self/requestSelfLrn.do"
-CANCEL_URL = "http://academic.petapop.com/self/deleteSelfLrn.do"
-
-
-KST = pytz.timezone('Asia/Seoul')
+# SIGNIN_URL = "http://academic.petapop.com/sign/actionLogin.do"
+# APPLY_URL = "http://academic.petapop.com/self/requestSelfLrn.do"
+# CANCEL_URL = "http://academic.petapop.com/self/deleteSelfLrn.do"
 
 
-def kstNow():
-    return datetime.utcnow().astimezone(KST)
+# KST = pytz.timezone('Asia/Seoul')
 
 
-def cleanUp(str):
-    stringList = str.split()
-    result = " ".join(stringList)
-    return result
+# def kstNow():
+#     return datetime.utcnow().astimezone(KST)
 
 
-def checkIfCredentialValid(id, password):
-    with req.session() as sess:
-        res = sess.post(SIGNIN_URL, data=userData)
-        # response = json.loads(res.content.decode('utf-8'))
-        rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
-
-        if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
-            return False
-
-        else:
-            return True
+# def cleanUp(str):
+#     stringList = str.split()
+#     result = " ".join(stringList)
+#     return result
 
 
-def applySelfLearn(body):
+# def checkIfCredentialValid(id, password):
+#     with req.session() as sess:
+#         res = sess.post(SIGNIN_URL, data=userData)
+#         # response = json.loads(res.content.decode('utf-8'))
+#         rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
 
-    userData = {
-        "id": body['id'],
-        "password": body['password']
-    }
+#         if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
+#             return False
 
-    applyData = {
-        'roomTcherId': body['homeRoomTeacherCode'],
-        'cchTcherId': body['conductingTeacherCode'],
-        'clssrmId': body['classroomCode'],
-        'actCode': body['actCode'],
-        'actCn': body['actContent'],
-        'sgnId': kstNow().strftime(r"%Y%m%d")
-    }
-
-    print("logging in")
-
-    with req.session() as sess:
-        res = sess.post(SIGNIN_URL, data=userData)
-        rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
-
-        if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
-            return "Failed to log in", 400
-
-        else:
-            print("logged in")
-            result = []
-
-            for period in body['periods']:
-                print(f"applying for {period}")
-                applyCaseData = applyData.copy()
-                applyCaseData['lrnPd'] = period
-
-                res = sess.post(APPLY_URL, data=applyCaseData)
-                response = json.loads(res.content.decode('utf-8'))
-
-                if response['result']['success'] == True:
-                    result.append(response['slrnNo'])
-
-                else:
-                    result.append(-1)
-
-            return result
+#         else:
+#             return True
 
 
-userData = {
-    "id": "enc2586",
-    "password": "rhkgkrrh1!"
-}
+# def applySelfLearn(body):
+
+#     userData = {
+#         "id": body['id'],
+#         "password": body['password']
+#     }
+
+#     applyData = {
+#         'roomTcherId': body['homeRoomTeacherCode'],
+#         'cchTcherId': body['conductingTeacherCode'],
+#         'clssrmId': body['classroomCode'],
+#         'actCode': body['actCode'],
+#         'actCn': body['actContent'],
+#         'sgnId': kstNow().strftime(r"%Y%m%d")
+#     }
+
+#     print("logging in")
+
+#     with req.session() as sess:
+#         res = sess.post(SIGNIN_URL, data=userData)
+#         rawPage = BeautifulSoup(res.content.decode('utf-8'), "html.parser")
+
+#         if cleanUp(rawPage.li.get_text()) == "선생님은 가입해주세요.":
+#             return "Failed to log in", 400
+
+#         else:
+#             print("logged in")
+#             result = []
+
+#             for period in body['periods']:
+#                 print(f"applying for {period}")
+#                 applyCaseData = applyData.copy()
+#                 applyCaseData['lrnPd'] = period
+
+#                 res = sess.post(APPLY_URL, data=applyCaseData)
+#                 response = json.loads(res.content.decode('utf-8'))
+
+#                 if response['result']['success'] == True:
+#                     result.append(response['slrnNo'])
+
+#                 else:
+#                     result.append(-1)
+
+#             return result
+
+
+# userData = {
+#     "id": "enc2586",
+#     "password": "rhkgkrrh1!"
+# }
 
 
 applyBody = {
@@ -98,5 +98,7 @@ applyBody = {
     "periods": [1, 2]
 }
 
-# print(checkIfCredentialValid(**userData)) #아이디 비번 유효 확인
-print(applySelfLearn(applyBody))
+# # print(checkIfCredentialValid(**userData)) #아이디 비번 유효 확인
+# # print(applySelfLearn(applyBody))
+
+print(applyBody['d'])
